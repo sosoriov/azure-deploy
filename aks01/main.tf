653c2f76ff66324ea894a94e1297cb8166cdb263
gitName <sebas212@gmail.com>
2021-03-10 16:59:20 +0100

    adding log analytics to aks

--- aks01/main.tf
+++ aks01/main.tf
@@ -21,7 +21,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   addon_profile {
     oms_agent {
-      enabled = true
+      enabled                    = var.enable_log_analytics_workspace
+      log_analytics_workspace_id = var.enable_log_analytics_workspace ? azurerm_log_analytics_workspace.main-workspace[0].id : null
     }
   }
 
@@ -35,6 +36,23 @@ resource "azurerm_log_analytics_workspace" "main-workspace" {
   resource_group_name = azurerm_resource_group.main.name
   sku                 = var.log_analytics_workspace_sku
   retention_in_days   = var.log_retention_in_days
+
+  tags = var.tags
+}
+
+resource "azurerm_log_analytics_solution" "main-solution" {
+  count               = var.enable_log_analytics_workspace ? 1 : 0
+  solution_name       = "ContainerInsights"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  workspace_name        = azurerm_log_analytics_workspace.main-workspace[0].name
+  workspace_resource_id = azurerm_log_analytics_workspace.main-workspace[0].id
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
 
   tags = var.tags
 
--- aks01/terraform.tfvars
+++
@@ -1,7 +0,0 @@
-resource_group = "rg-seboso01"
-
-cluster_name = "seboso01-aks
-
-tags = {
-  "environment" = "labo"
-}
